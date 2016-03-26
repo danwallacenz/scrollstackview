82114553594220f6e07ad851697015be8031fa9d
Dan Wallace <daniel.wallace@ikegps.com>
2016-03-27 12:30:23 +1300

    add scrollable stack view

--- interface-builder/ViewController.swift
+++ interface-builder/ViewController.swift
@@ -15,11 +15,17 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func add(sender: AnyObject) {
     }
 
+    @IBAction func save(sender: AnyObject) {
+    }
 
+    @IBAction func compose(sender: AnyObject) {
+    }
+    
+    @IBAction func takePhoto(sender: AnyObject) {
+    }
+    
 }
 
