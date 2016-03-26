3ea3d79996dcb2b4cb2b451e3cf68b3056e34d28
Dan Wallace <daniel.wallace@ikegps.com>
2016-03-26 21:43:39 +1300

    initial commit

---
+++ interface-builder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  interface-builder
+//
+//  Created by Daniel Wallace on 26/03/16.
+//  Copyright © 2016 IkeGPS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
